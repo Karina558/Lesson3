ba8ba47c1a6976801eadb14366dd201c7a11931f
Karina <kidiaievakarina@gmail.com>
2023-12-17 17:22:06 +0200

    ex-1 fixed

--- if_else_loop.py
+++ if_else_loop.py
@@ -1,11 +1,11 @@
 try:
     x1 = (int(input('Enter the first number: ')))
     x2 = (int(input('Enter the second number: ')))
+
     if x1>x2:
         x1,x2 = x2,x1
-    x = x1 - 1 + 1
     sum = 0
-    while x < x2 + 1:
+    for x in range(x1,x2+1):
         sum += x
         x += 1
     print('Sum of all integers between', x1, 'and', x2, 'including them is', sum)
