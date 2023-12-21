e4b250aa3f4a55e4f96ee244d65bd03ace55460b
Karina <kidiaievakarina@gmail.com>
2023-12-22 00:37:28 +0200

    lab ex-4 done

--- if_else_loop.py
+++ if_else_loop.py
@@ -1,8 +1,9 @@
 try:
     n = int(input('Введіть довжину рядка: '))
+    m = str(input('Введіть символ для рядка: '))
     number = 1
     while number < n+1:
-        print ('*', end ='')
+        print (m, end ='')
         number += 1
 
 except Exception as e:
