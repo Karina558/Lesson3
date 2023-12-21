796af83b8dfbdf17f126d0c5d73d2ed5375ca3d7
Karina <kidiaievakarina@gmail.com>
2023-12-21 22:35:41 +0200

    lab ex-1 done

--- if_else_loop.py
+++ if_else_loop.py
@@ -2,12 +2,19 @@ try:
     x1 = (int(input('Enter the first number: ')))
     x2 = (int(input('Enter the second number: ')))
 
-    if x1>x2:
-        x1,x2 = x2,x1
+    if x1 > x2:
+        x1, x2 = x2, x1
     sum = 0
-    for x in range(x1,x2+1):
+    for x in range(x1, x2 + 1):
         sum += x
         x += 1
-    print('Sum of all integers between', x1, 'and', x2, 'including them is', sum)
+
+    print('Sum of all integers between', x1, 'and', x2, 'including them is: ', sum)
+
+    numbers = list(range(x1, x2+1))
+    count = len (numbers)
+    avg = (sum / count)
+    print ('Average of all integers between', x1, 'and', x2, 'including them is: ', avg)
+
 except Exception as e:
     print (e)
