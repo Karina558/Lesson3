f3214d89ba591d8d45921ddedab13ecb909565a4
Karina <kidiaievakarina@gmail.com>
2023-12-22 04:00:23 +0200

    dz ex-2 done

--- if_else_loop.py
+++ if_else_loop.py
@@ -1,15 +1,26 @@
 try:
-    x1 = int(input('Ведіть число початку діапазону: '))
-    x2 = int(input('Ведіть число кінця діапазону: '))
-    found = False
+    x1 = int(input('Введіть початок діапазону: '))
+    x2 = int(input('Введіть кінець діапазону: '))
+    count = 0
 
-    print('Числа кратні 7:', end=' ')
+    print('Числа у даному діапазоні:')
+    for i in range (x1, x2+1):
+        print (i, end='; ' if i < x2 else '.')
 
-    for i in range(x1, x2+1):
-        if i % 7 ==0:
-            print (i, end='; ')
-            found = True
-    if not found:
-        print(' ! у діапазоні від', x1, 'до', x2, 'відсутні числа кратні 7')
+    print("\nЧисла у діапазоні в зворотньому порядку:")
+    for i in range (x1, x2+1) [::-1]:
+        print (i, end='.' if i < x1+1 else '; ')
+
+    print("\nЧисла кратні 7:")
+    for i in range(x1, x2 + 1):
+        if i % 7 == 0:
+            print(i, end='; ' if i < x2-6 else '.')
+
+    print("\nКількість чискл кратних 5:")
+    for i in range(x1, x2 + 1):
+        if i % 5 == 0:
+            count += 1
+    print(count, end='.')
+
 except Exception as e:
     print (e)
