4b6f13b73ba1b446c4ea8646c2e400186221c8f5
Karina <kidiaievakarina@gmail.com>
2023-12-22 02:25:09 +0200

    dz ex-1 done

--- if_else_loop.py
+++ if_else_loop.py
@@ -1,10 +1,15 @@
 try:
-    n = int(input('Введіть довжину рядка: '))
-    m = str(input('Введіть символ для рядка: '))
-    number = 1
-    while number < n+1:
-        print (m, end ='')
-        number += 1
+    x1 = int(input('Ведіть число початку діапазону: '))
+    x2 = int(input('Ведіть число кінця діапазону: '))
+    found = False
 
+    print('Числа кратні 7:', end=' ')
+
+    for i in range(x1, x2+1):
+        if i % 7 ==0:
+            print (i, end='; ')
+            found = True
+    if not found:
+        print(' ! у діапазоні від', x1, 'до', x2, 'відсутні числа кратні 7')
 except Exception as e:
     print (e)
